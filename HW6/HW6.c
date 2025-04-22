82637da98651167f9ed813c99516e55bf77ee32b
Bylin-code <85825704+Bylin-code@users.noreply.github.com>
2025-04-21 21:17:57 -0500

    Fnished HW6

--- HW6/HW6.c
+++ HW6/HW6.c
@@ -2,51 +2,52 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 12
 #define I2C_SCL 13
-// I2C address of the device
-#define MY_ADDR 0b00100000
+#define MY_ADDR 0x20  // 7-bit address (A2:A0 = GND)
 
-// MCP23008 Registers
-// Register addr for IO direction
-#define IODIR   0x00
-// Register addr for GPIO 0 
-#define GPIO    0x09 
-
-int btn_state;
+// IO direction address and address for GPIO
+#define IODIR  0x00
+#define GPIO   0x09
+#define OLAT   0x0A
 
 void chip_write(uint8_t reg, uint8_t value) {
     uint8_t buffer[2] = { reg, value };
     i2c_write_blocking(I2C_PORT, MY_ADDR, buffer, 2, false);
 }
 
-int main()
-{
+uint8_t chip_read(uint8_t reg) {
+    uint8_t value;
+    i2c_write_blocking(I2C_PORT, MY_ADDR, &reg, 1, true);   // Write register address
+    i2c_read_blocking(I2C_PORT, MY_ADDR, &value, 1, false); // Read data
+    return value;
+}
+
+int main() {
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
+    i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
-    
-    // initialize the MCP23008 to set GPIO 0 as output
-    chip_write(IODIR, 0b11111110);
+
+    sleep_ms(500);  
+
+    // GP1 = input, GP0 = output
+    chip_write(IODIR, 0b11111110);  // Bit 1 = 1 (GP1 input), Bit 0 = 0 (GP0 output)
 
     while (true) {
+        uint8_t gpio_val = chip_read(GPIO);
+        bool btn_pressed = (gpio_val & (1 << 1));  // GP1 is bit 1
+        if (btn_pressed) {
+            chip_write(GPIO, 0b00000001);  // Set GP0 high
+        }
+        else {
+            chip_write(OLAT, 0b00000000);  // Set GP0 low
+        }
 
-        btn_state = i2c_read_blocking(i2c_default, MY_ADDR, 0b00000010, 1, false);  // false - finished with bus
-
-        chip_write(GPIO, 0b00000001);
-        sleep_ms(1000);
-        chip_write(GPIO, 0b00000000);
-        sleep_ms(1000);
+        sleep_ms(10);
     }
 }
